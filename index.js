fc5c4a73570556f4972ecb25e4a0217521776962
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2019-04-25 10:20:47 +0300

    passport and session work on localhost

--- index.js
+++ index.js
@@ -5,32 +5,58 @@ const fs      = require('fs');
 const https   = require('https');
 const app = express();
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
+passport.use(new Strategy(
   (username, password, done) => {
+    console.log(`login? ${username}`);
     //Normally, select * from users where username=?
     if(username != process.env.USR && password != process.env.PWD) {
+      console.log('login failed');
        return done(null, false);
     }
-    return done(null, user);
+    console.log('login ok');
+    return done(null, {name: username});
   }
 ));
+passport.serializeUser((user, done) => {
+  console.log('session serialize');
+    done(null, user);
+});
+ 
+passport.deserializeUser((user, done) => {
+  console.log('session deserialize');
+  done(null, user);
+});
+
+app.use(require('serve-static')(__dirname + '/public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: process.env.SEC, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 console.log('Alive we ride');
 
-const sslkey  = fs.readFileSync('/etc/pki/tls/private/ca.key');
-const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+const sslkey  = fs.readFileSync(process.env.KEY);
+const sslcert = fs.readFileSync(process.env.CERT);
 const options = {
     key: sslkey,
     cert: sslcert
 };
 
 app.get('/', (req, res) => {
-  if (req.secure) res.send('https :)');
+  console.log(req);
+  if (req.secure) res.send('https :) and hello' + req.user.name);
   else res.send('hello not secure?');
 });
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
 app.post('/',
-  bodyParser.urlencoded({extended:true}),
   (req, res) => {
   console.log(req.body);
   res.send('POST: Hello ' + req.body.name);
